bbc8e3fdc5191c4af25fc6c2a07237919cdd523c
limseunghyun95 <limseunghyun95@gmail.com>
2023-07-15 15:41:51 +0900

    Update create subnet code

--- week02-assignment/01-create-subnet/README.md
+++ week02-assignment/01-create-subnet/README.md
@@ -1,1 +1,1 @@
-aws_availability_zones, aws_vpc를 사용해서 default VPC내 subnet 생성하기
+VPC를 생성하고 해당 VPC내 subnet 생성하기
--- week02-assignment/01-create-subnet/main.tf
+++ week02-assignment/01-create-subnet/main.tf
@@ -2,14 +2,18 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-data "aws_vpc" "default" {
-  default = true
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "my-vpc"
+  }
 }
 
 resource "aws_subnet" "primary" {
-  vpc_id            = data.aws_vpc.default.id
+  vpc_id            = aws_vpc.my_vpc.id
   availability_zone = data.aws_availability_zones.available.names[0]
-  cidr_block        = "172.31.100.0/24"
+  cidr_block        = "10.0.100.0/24"
 
   tags = {
     Name = "primary"
@@ -17,9 +21,9 @@ resource "aws_subnet" "primary" {
 }
 
 resource "aws_subnet" "secondary" {
-  vpc_id            = data.aws_vpc.default.id
+  vpc_id            = aws_vpc.my_vpc.id
   availability_zone = data.aws_availability_zones.available.names[0]
-  cidr_block        = "172.31.101.0/24"
+  cidr_block        = "10.0.101.0/24"
 
   tags = {
     Name = "secondary"
